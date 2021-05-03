3cf8595c2a6f21e52e31da7442175228ee5b5ae8
Ben Vickers <bvickers@vmware.com>
2021-05-03 14:45:42 +0200

    simplify handling of key

--- .github/scripts/gitops-deploy.sh
+++ .github/scripts/gitops-deploy.sh
@@ -2,7 +2,9 @@
 set -euo pipefail
 echo 'Deploying....'
 
-cd / && GIT_SSH_COMMAND='ssh -i <(echo "$RPI_GIT_DEPLOY_KEY") -o IdentitiesOnly=yes -o StrictHostKeyChecking=no' git clone git@github.com:benjvi/rpi-k8s.git
+KEYFILE=mktemp
+echo "$RPI_GIT_DEPLOY_KEY" > "$KEYFILE"
+cd / && GIT_SSH_COMMAND='ssh -i "$KEYFILE" -o IdentitiesOnly=yes -o StrictHostKeyChecking=no' git clone git@github.com:benjvi/rpi-k8s.git
 
 IMG_VERSION="$(git rev-parse --short=8 HEAD)"
 # TODO: add kshard in here
@@ -15,5 +17,5 @@ cd -
 cd rpi-k8s/sync/prod
 git config user.email "benjvi.github.io@ghactions"
 git config user.name "GH Actions CI Bot - Blog"
-GIT_SSH_COMMAND=\'ssh -i $SSH_KEY_FOR_GITOPS -o IdentitiesOnly=yes -o StrictHostKeyChecking=no\' prify run'
-              
+GIT_SSH_COMMAND=\'ssh -i "$KEYFILE" -o IdentitiesOnly=yes -o StrictHostKeyChecking=no\' prify run
+rm $KEYFILE 
