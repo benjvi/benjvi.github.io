6d054212340c400d65e97dc8459b9756eaec8080
Ben Vickers <bvickers@vmware.com>
2021-05-03 15:10:55 +0200

    fix paths

--- .github/scripts/gitops-deploy.sh
+++ .github/scripts/gitops-deploy.sh
@@ -10,11 +10,11 @@ IMG_VERSION="$(git rev-parse --short=8 HEAD)"
 # TODO: add kshard in here
 cd deploy/k8s
 kustomize edit set image "benjvi/blog-arm=benjvi/blog-arm:${IMG_VERSION}"
-kustomize build . > rpi-k8s/sync/prod/blog/k8s-blog/package.yml
+kustomize build . > /rpi-k8s/sync/prod/blog/k8s-blog/package.yml
 # need some details set in env for prify to work correctly
 # not all context is kept between sh commands, so use a one liner
 cd -
-cd rpi-k8s/sync/prod
+cd /rpi-k8s/sync/prod
 git config user.email "benjvi.github.io@ghactions"
 git config user.name "GH Actions CI Bot - Blog"
 GIT_SSH_COMMAND=\'ssh -i "$KEYFILE" -o IdentitiesOnly=yes -o StrictHostKeyChecking=no\' prify run
